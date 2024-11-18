012aba4524a8d47e9bb4823b4462f97094215cd6
eamun <eamongahran@gmail.com>
2024-11-18 23:51:24 +0000

    Added the Invalid palette size test

--- src/main/java/intentions/ColourTable.java
+++ src/main/java/intentions/ColourTable.java
@@ -3,7 +3,7 @@ package intentions;
 
 public class ColourTable {
     public ColourTable(int size) {
-        if (size <= 1 ||  (size & (size - 1)) != 0) { //bitwise and as all powers of 2 contain exactly 1 bit set to 1
+        if (size <= 1 ||(size & (size - 1)) != 0) { //bitwise and as all powers of 2 contain exactly 1 bit set to 1
             throw new IllegalArgumentException("palette size must be a power of two .");
         }
     }
--- src/test/java/intentions/TestColourTable.java
+++ src/test/java/intentions/TestColourTable.java
@@ -1,12 +1,22 @@
 package intentions;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class TestColourTable {
-    @Test
-    void TestPaletteSize() {
-        assertDoesNotThrow(() -> new ColourTable(16));
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 8, 16, 32, 2048})//valid palette sizes
+    void TestValidPaletteSize(int size) {
+        assertDoesNotThrow(() -> new ColourTable(size));
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 15, 0, -4, -1})//invalid palette sizes
+    void TestInvalidPaletteSize(int size){
+        assertThrows(IllegalArgumentException.class,() -> new ColourTable(size));
+    }
+
 }
